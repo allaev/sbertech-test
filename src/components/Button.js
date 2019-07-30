3f700f5ed8379994da9a4ec5ac371ed9caf3015b
allaev <34710484+allaev@users.noreply.github.com>
2019-07-30 10:43:48 +0300

    refactor Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,6 @@ import './Button.css';
 
 export default class Button extends Component {
   render() {
-    return <div className="Button">
-      <button onClick = { () => this.props.onClick() }>Запуск</button>
-    </div>;
+    return <button className="Button" onClick = { () => this.props.onClick() }>Запуск</button>;
   }
 };
